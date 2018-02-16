70a7d4d0753ecbd5d0af3c6e47fafbaddc03b2b4
Jake Schurch <jschurch@bryant.edu>
2018-02-16 10:58:27 -0500

    fix git branch

--- main.py
+++ main.py
@@ -4,26 +4,27 @@ import time
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
-from tweepy import API, api
+from tweepy import API
 import json
 
 
 class Listener(StreamListener):
     def __init__(self, start_time, time_limit):
-
+        """ Initializer for Listener Class """
         self.time = start_time
         self.limit = time_limit
         self.tweet_counter = 0
         self.data = []
 
     def on_data(self, tweet):
-
+        """ When data comes in, this appends the data and adds to counter """
         self.data.append(tweet)
 
         self.tweet_counter += 1
         print("{0} Tweet/s Downloaded".format(self.tweet_counter))
 
     def store_data(self):
+        """ Writes data to JSON file """
         file.write(json.dumps(self.data))
 
 
@@ -32,14 +33,15 @@ def setup_auth(
         consumer_secret='rS4KyDgoz1MCRIeMVdwOqRD706S0cC5jCvxoYTsINbjWCZLl6f',
         access_token_key='958003558695276544-eUvZUiT2nRfiUWSZjpGGXjYJueh8Khh',
         access_token_secret='7ter6ZZGa9W7Vr3qBqfFwFB36sUQj8g7EQ9KneNJC5IaZ'):
-
+    """ Sets up authentication for Twitter API """
     auth = OAuthHandler(ckey, consumer_secret)  # OAuth object
     auth.set_access_token(access_token_key, access_token_secret)
 
     return auth
 
 
-def init(jsonFile='twitterOutput.json', time_limit=30):
+def setup(jsonFile='twitterOutput.json', time_limit=30):
+    """ Setup variables for __main__ function """
     global file
     global auth
     global api
@@ -52,33 +54,42 @@ def init(jsonFile='twitterOutput.json', time_limit=30):
 
 
 def get_tweet_stream(keywords: list):
+    """ Opens connection with API for Stream Listener """
 
     # initialize Stream object with a time out limit
     twitterStream = Stream(auth, listener)
     twitterStream.filter(track=keywords, languages=['en'])
 
 
-def read_json(filename: str):
-
+def read_json_file(filename: str):
+    """ Read in a JSON file """
     with open(filename, 'r') as f:
-        for line in f:
-            data = json.loads(line)
-
-        data = [json.loads(d) for d in data]
-        return data
+        for l in f:
+            data = json.loads(l)
+            return data
 
 
 def get_tweet_by_id(tweet_ID):
+    """ Gets a specific tweet by ID """
     tweet = api.get_status(tweet_ID)
     file.write(json.dumps(tweet._json))
 
 
 def get_tweets_by_user(screen_name):
-    # 200 is the maximum allowed count
+    """ Gets most recent up to 200 (maximum allowed by API) tweets by user """
     new_tweets = api.user_timeline(screen_name=screen_name, count=200)
 
     for tweet in new_tweets:
         file.write(json.dumps(tweet._json))
+
+
+class tweet():
+    def __init__(self, tweetId, createdAt, text, userId):
+        self.tweetId = tweetId
+        self.createdAt = createdAt
+        self.text = text
+        self.userId = userId
+
 
 def testing():
     """
@@ -98,6 +109,12 @@ def testing():
         for v in vals:
             print(v)
 
+def read_json(filename: str):
+    with open(filename, 'r') as f:
+        for line in f:
+            data = json.loads(line)
+        data = [json.loads(d) for d in data]
+    return data
 
 def load_json(filename: str):
     data = read_json(filename)
