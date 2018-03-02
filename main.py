5ec6563272eeabd8340a4e011dd01a2694affef9
Jake Schurch <jschurch@bryant.edu>
2018-03-02 12:45:49 -0500

    Develop Functionality for Reading back JSON Dictionaries (#4)
    * Add to_csv functionality
    
    * /jsonPull: Add jsonPull functions
    
    Allow for further functionality with reading serialized JSON objects to string and back from file.

--- main.py
+++ main.py
@@ -5,27 +5,90 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 from tweepy import API
-import json
+
+import simplejson as json
+import csv
 
 
 class Listener(StreamListener):
-    def __init__(self, start_time, time_limit):
+    def __init__(self, time_limit, tweet_limit=1):
         """ Initializer for Listener Class """
-        self.time = start_time
+        self.time = time.time.now()
         self.limit = time_limit
         self.tweet_counter = 0
+        self.tweet_limit = tweet_limit
         self.data = []
+        self.StreamObj = None
 
     def on_data(self, tweet):
         """ When data comes in, this appends the data and adds to counter """
-        self.data.append(tweet)
+        if self.tweet_counter <= self.tweet_limit:
+            self.data.append(tweet)
 
-        self.tweet_counter += 1
-        print("{0} Tweet/s Downloaded".format(self.tweet_counter))
+            self.tweet_counter += 1
+            print("{0} Tweet/s Downloaded".format(self.tweet_counter))
+        else:
+            self.StreamObj.disconnect()
+            results.extend(self.data[:-1])
 
-    def store_data(self):
-        """ Writes data to JSON file """
-        file.write(json.dumps(self.data))
+
+class tweet():
+    def __init__(self, tweetId, createdAt, text, userId):
+        self.tweetId = tweetId
+        self.createdAt = createdAt
+        self.text = text
+        self.userId = userId
+
+    def to_record(self) -> list:
+        return [self.tweetId, self.createdAt, self.text, self.userId]
+
+
+def get_tweet_by_id(tweet_ID, api):
+    """ Gets a specific tweet by ID """
+    tweet = api.get_status(tweet_ID)
+    results.extend(tweet._json)
+
+
+def get_tweets_by_user(screen_name, api, count=200):
+    """ Gets most recent up to 200 (maximum allowed by API) tweets by user """
+    new_tweets = api.user_timeline(screen_name=screen_name, count=200)
+
+    for tweet in new_tweets:
+        results.extend(tweet._json)
+
+
+def to_csv(tweet_list: list, file_name='tweet_output.csv', delim=','):
+    with open(file_name, "a") as file:
+        csv_writer = csv.writer(file, delimiter=delim)
+
+        # write headers to file
+        csv_writer.writerow(['tweetId', 'createdAt', 'text', 'userId'])
+        for tweet_value in tweet_list:
+            csv_writer.writerow(tweet_value.__dict__.values())
+
+
+def write_json(data: list, file, overwrite: str):
+    data = json.dumps(data, ensure_ascii=False, separators=(',', ': '))
+
+    if overwrite is True:
+        filemode = 'w'
+    else:
+        filemode = 'a'
+    with open(file, filemode) as f:
+        f.write(data)
+
+
+def read_json(file: str):
+    with open(file, 'r') as f:
+        data = json.load(f, encoding='utf8')
+    return data
+
+
+def get_tweet_stream(keywords: list, auth, listener: Listener):
+    """ Opens connection with API for Stream Listener """
+    twitterStream = Stream(auth, listener)
+    listener.StreamObj = twitterStream
+    twitterStream.filter(track=keywords, languages=['en'])
 
 
 def setup_auth(
@@ -37,74 +100,35 @@ def setup_auth(
     auth = OAuthHandler(ckey, consumer_secret)  # OAuth object
     auth.set_access_token(access_token_key, access_token_secret)
 
-    return auth
-
-
-def setup(jsonFile='twitterOutput.json', time_limit=30):
-    """ Setup variables for __main__ function """
-    global file
-    global auth
-    global api
-    global listener
-
-    file = open(jsonFile, 'a')
-    auth = setup_auth()
     api = API(auth)
-    listener = Listener(time.time(), time_limit)
+    return auth, api
 
 
-def get_tweet_stream(keywords: list):
-    """ Opens connection with API for Stream Listener """
+def main(keywords: list,
+         filename: str,
+         byId=False,
+         byUser=False,
+         byStream=False,
+         timeLimit=None,
+         tweetLimit=50,
+         overwrite=False):
+    global results
+    results = []
 
-    # initialize Stream object with a time out limit
-    twitterStream = Stream(auth, listener)
-    twitterStream.filter(track=keywords, languages=['en'])
+    auth, api = setup_auth()
 
+    if byStream is True:
+        listener = Listener(timeLimit, tweetLimit)
+        get_tweet_stream(keywords, listener)
+    if byId is True:
+        for kw in keywords:
+            get_tweet_by_id(kw, api)
+    if byUser is True:
+        for kw in keywords:
+            get_tweets_by_user(kw, api, tweetLimit)
 
-def read_json_file(filename: str):
-    """ Read in a JSON file """
-    with open(filename, 'r') as f:
-        for l in f:
-            data = json.loads(l)
-            return data
-
-
-def get_tweet_by_id(tweet_ID):
-    """ Gets a specific tweet by ID """
-    tweet = api.get_status(tweet_ID)
-    file.write(json.dumps(tweet._json))
-
-
-def get_tweets_by_user(screen_name):
-    """ Gets most recent up to 200 (maximum allowed by API) tweets by user """
-    new_tweets = api.user_timeline(screen_name=screen_name, count=200)
-
-    for tweet in new_tweets:
-        file.write(json.dumps(tweet._json))
-
-
-class tweet():
-    def __init__(self, tweetId, createdAt, text, userId):
-        self.tweetId = tweetId
-        self.createdAt = createdAt
-        self.text = text
-        self.userId = userId
-
-def read_json(filename: str):
-    with open(filename, 'r') as f:
-        for line in f:
-            data = json.loads(line)
-        data = [json.loads(d) for d in data]
-    return data
+    write_json(results, filename, overwrite)
 
 
 if __name__ == "__main__":
-    setup()
-    # testing()
-    try:
-    #     # get_tweet_by_id(959393270144086016)
-    #     # get_tweets_by_user('realDonaldTrump')
-        get_tweet_stream(['Trump'])
-    finally:
-        listener.store_data()
-        file.close()
+    main(['RealDonaldTrump'], 'twitterOutput.json', byUser=True)
